e7fe56fb5f35e93c682670fe2103f76d4657269c
pisa-kun <pisa0401mst@gmail.com>
2024-12-10 14:25:44 +0900

    fix: setting port

--- 01-helloworld-cloud-run/provider.tf
+++ 01-helloworld-cloud-run/provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("C:\\Users\\pisa0\\AppData\\Roaming\\gcloud\\application_default_credentials.json")  # サービスアカウントの認証ファイル
   project     = var.project_id
   region      = var.region
 }
